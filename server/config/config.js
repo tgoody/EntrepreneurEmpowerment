d5525a0cc0bd04a1ed9f677bf9cec144c66443a3
Debrian Figueroa <debrianfigueroa@ufl.edu>
2019-02-09 12:54:01 -0500

    added procfile

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,14 @@
 //This file holds any configuration variables we may need 
 //'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
 module.exports = {
   db: {
     uri: 'mongodb://admin:adminPassword1@ds141704.mlab.com:41704/bootcamp3', //place the URI of your mongo database here.
   }, 
-  port: 8080
+  port: port
 };
