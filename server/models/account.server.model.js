a70afcd314848dce6c8199e75798b4c3981e4122
Tyler Goodman <goodmane@ufl.edu>
2019-03-05 15:55:25 -0600

    I brought back the account schema because Im an idiot

--- server/models/account.server.model.js
+++ server/models/account.server.model.js
@@ -1,54 +1,45 @@
 /* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+Schema = mongoose.Schema;
 
+/* Create your schema */
+var accountSchema = new Schema({
+                               email: {
+                               type: String,
+                               required: true
+                               },
+                               username: {
+                               type: String,
+                               required: true,
+                               unique: true
+                               },
+                               password: {
+                               type: String,
+                               required: true
+                               },
+                               admin: false,
+                               created_at: Date,
+                               updated_at: Date
+                               });
 
-var commentSchema = new Schema({
-
-    username: String,
-    commentText: String,
-    created_at: Date,
-    updated_at: Date
-});
-
-
-var videoSchema = new Schema({
-  link: {
-    type: String,
-    required: true
-  },
-  forUsers: boolean,
-  comments: [commentSchema],
-  created_at: Date,
-  updated_at: Date
-});
-
-
-videoSchema.pre('save', function(next) {
-  var currentTime = new Date;
-  this.updated_at = currentTime;
-  if(!this.created_at)
-  {
-    this.created_at = currentTime;
-  }
-  next();
-});
-
-commentSchema.pre('save', function(next) {
-    var currentTime = new Date;
-    this.updated_at = currentTime;
-    if(!this.created_at)
-    {
-        this.created_at = currentTime;
-    }
-    next();
-});
-
-
+/* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
+/* Added functionality
+ - Sets admin status to false
+ - Hashes password.
+ */
+accountSchema.pre('save', function(next) {
+                  var currentTime = new Date;
+                  this.updated_at = currentTime;
+                  this.admin = false;
+                  if(!this.created_at)
+                  {
+                  this.created_at = currentTime;
+                  }
+                  next();
+                  });
 
 /* Use your schema to instantiate a Mongoose model */
-var Video = mongoose.model('Video', videoSchema);
-var Comment = mongoose.model('Comment', commentSchema);
-/* Export the model to make it avaiable to other parts of your Node application */
-module.exports = {Video: Video, Comment: Comment};
+var Account = mongoose.model('Account', accountSchema);
 
+/* Export the model to make it avaiable to other parts of your Node application */
+module.exports = Account;
