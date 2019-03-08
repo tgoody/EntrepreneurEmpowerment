65d56ebf2ef07a036978a78947c19b35cc7b3582
CarlRiley99 <32286532+CarlRiley99@users.noreply.github.com>
2019-03-08 15:39:35 -0500

    Changed Database

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,16 @@
-//This file holds any configuration variables we may need 
+//This file holds any configuration variables we may need
 //'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
 
 let port = process.env.PORT;
+let testDataBase = 'mongodb://Carl:123abc@ds349175.mlab.com:49175/registering_accounts_test';
+let realDataBase = 'mongodb://root:notr00t@ds249035.mlab.com:49035/entrepreneurempowerment';
 if (port == null || port == "") {
   port = 8080;
 }
 
 module.exports = {
   db: {
-    uri: 'mongodb://root:notr00t@ds249035.mlab.com:49035/entrepreneurempowerment', //place the URI of your mongo database here.
-  }, 
+    uri: testDataBase, //place the URI of your mongo database here.
+  },
   port: port
 };
