80d64203c0a04ff311057382b053bbaddbe1e54f
CarlRiley99 <32286532+CarlRiley99@users.noreply.github.com>
2019-03-11 16:16:50 -0400

    Updated listingFactory for register

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,17 +1,17 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://localhost:8080/api/listings');
+      return $http.get('http://localhost:8080/api/accounts');
     },
 
     create: function(listing) {
-      return $http.post('http://localhost:8080/api/listings', listing);
+      return $http.post('http://localhost:8080/api/accounts', listing);
       },
 
     delete: function(id) {
       /* return result of HTTP delete method
         */
-      return $http.delete('http://localhost:8080/api/listings' + id);
+      return $http.delete('http://localhost:8080/api/accounts/' + id);
     },
 
 //------------------------------------------------------------------------------//
