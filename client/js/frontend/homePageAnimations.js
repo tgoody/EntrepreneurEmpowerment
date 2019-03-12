63d2c36c0b6f114f2cb683a66e9862f1c965d6b0
Debrian Figueroa <debrianfigueroa@ufl.edu>
2019-03-11 20:27:31 -0400

    adjust css on register page

--- client/js/frontend/homePageAnimations.js
+++ client/js/frontend/homePageAnimations.js
@@ -1,13 +1,4 @@
 jQuery(function ($) {
-
-  //#main-slider
-  $(function () {
-    $('#main-slider.carousel').carousel({
-      interval: 8000
-    });
-  });
-
-
   //Initiat WOW JS
   new WOW().init();
 
