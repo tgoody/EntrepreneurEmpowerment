b74fbc97b492b546c6ff568cbc944a89f11b63f5
CarlRiley99 <32286532+CarlRiley99@users.noreply.github.com>
2019-03-26 14:40:38 -0400

    test

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -1,3 +1,25 @@
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+
+
+  // Do not use a user's email address or user ID to communicate the currently
+  // signed-in user to your app's backend server. Instead, send the user's ID token
+  // to your backend server and validate the token on the server, or use the server
+  // auth code flow.
+
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  // console.log('Family Name: ' + profile.getFamilyName());
+  // console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+}
+
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     /* Get all the listings, then bind it to the scope */
@@ -8,28 +30,6 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     });
 
     $scope.detailedInfo = undefined;
-
-    $scope.googleSignIn = function(id_token) {
-      Listings.create(id_token).then(function(response) {
-        if (response.status === 200) {
-          // response was successful, refresh list
-          Listings.getAll().then(function(response) {
-            $scope.accountList = response.data;
-          }, function(error) {
-            console.log('Unable to retrieve listings:', error);
-          });
-          // Clear form inputs
-          $scope.account.name = '';
-          $scope.account.code = '';
-          $scope.account.address = '';
-        } else {
-
-        }
-      }, function(error) {
-        console.log('Unable to add new listing:', error);
-      });
-      $windows.location.href = '../../index.html';
-    };
 
     $scope.addAccount = function() {
       if ($scope.account.password !== $scope.confirmPassword) {
