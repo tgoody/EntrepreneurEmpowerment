0b6c14e50b45209558cdaf1df92ccdb4b7335d80
CarlRiley99 <32286532+CarlRiley99@users.noreply.github.com>
2019-03-31 19:10:06 -0400

    Verify/Delete Events

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -51,6 +51,10 @@ angular.module('listings', []).factory('Listings', function($http) {
     return $http.put('/calendar/' + id);
   },
 
+  deleteEvent: function(id) {
+    return $http.delete('/calendar/' + id);
+  },
+
   getEvents: function() {
     return $http.get('/calendar/events');
   },
