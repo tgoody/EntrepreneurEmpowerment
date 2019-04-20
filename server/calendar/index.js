4a91e7501f168fab45522ba42b876c32a45deec7
Debrian Figueroa <debrianfigueroa@ufl.edu>
2019-04-20 15:09:47 -0400

    pulling

--- server/calendar/index.js
+++ server/calendar/index.js
@@ -72,6 +72,44 @@ function getAccessToken(oAuth2Client, callback) {
  */
 function listEvents(auth) {
   const calendar = google.calendar({version: 'v3', auth});
+  var event = {
+    'summary': 'Events 2',
+    'location': '800 Howard St., San Francisco, CA 94103',
+    'description': 'New Event',
+    'start': {
+      'dateTime': '2019-05-04T09:00:00-07:00',
+      'timeZone': 'America/Los_Angeles',
+    },
+    'end': {
+      'dateTime': '2019-05-28T17:00:00-07:00',
+      'timeZone': 'America/Los_Angeles',
+    }
+    // 'recurrence': [
+    //   'RRULE:FREQ=DAILY;COUNT=2'
+    // ],
+    // 'attendees': [
+    //   {'email': 'lpage@example.com'},
+    //   {'email': 'sbrin@example.com'},
+    // ],
+    // 'reminders': {
+    //   'useDefault': false,
+    //   'overrides': [
+    //     {'method': 'email', 'minutes': 24 * 60},
+    //     {'method': 'popup', 'minutes': 10},
+    //   ],
+    // },
+  };
+  calendar.events.insert({
+    auth: auth,
+    calendarId: 'primary',
+    resource: event,
+  }, function(err, event) {
+    if (err) {
+      console.log('There was an error contacting the Calendar service: ' + err);
+      return;
+    }
+    console.log('Event created: %s', event.data.htmlLink);
+  });
   calendar.events.list({
     calendarId: 'primary', //entrempowerment@gmail.com
     timeMin: (new Date()).toISOString(),
